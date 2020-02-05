cce9cbd7bfa3747bdd875e2717172bc5f4785d5a
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-05 20:11:34 +0100

    add signup and signin forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root "welcome#home"
   resources :sessions
-  # resources :users
+  resources :users
 
   get "signup", to: "users#new"
   get "signin", to: "sessions#new"
+  post 'login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
