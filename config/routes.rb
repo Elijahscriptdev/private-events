2572b5cfe1ab9d6487a7598a2e1e76028c7f17c9
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-08 23:05:42 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  root "welcome#home"
+  # root "welcome#home"
   resources :sessions
   resources :users
-  resources :events
+  # resources :events
+  root 'events#index'
+
   get "signup", to: "users#new"
   get "signin", to: "sessions#new"
   post 'login', to: 'sessions#create'
+
+  get  'events', to: 'events#new'
+  post 'events', to: 'events#create'
+  get  'events', to: 'events#show'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
