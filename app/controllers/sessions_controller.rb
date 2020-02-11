52cc5dcd4784fbd00311316ba97d2f433e1ca333
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-11 14:57:01 +0100

    redirect signin and login to root path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,8 @@ class SessionsController < ApplicationController
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id
             flash[:success] = "You have successfully logged in"
-            redirect_to user_path(user)
+            # redirect_to user_path(user)
+            redirect_to root_path
         else
             flash.now[:danger] = "There is something wrong with your login information"
             render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash.now[:info] = "Welcome to the Private Events #{@user.username}"
-      redirect_to user_path(@user)
+      # redirect_to user_path(@user)
+      redirect_to root_path
     else
       render 'new'
     end
