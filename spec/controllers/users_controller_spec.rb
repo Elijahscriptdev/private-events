f599b6285f781c13a2bb1b9088cd06d35158ab25
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-17 18:43:51 +0100

    add test for controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :controller do
+  it { should route(:get, '/users').to(action: :index) }
+  it { should route(:get, '/users/1').to(action: :show, id: 1) }
+end
+
---
+++ spec/feature/signup.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe ArticlesController, type: :controller do
+
+    # Add this
+    login_user
+
+    let(:valid_attributes) {
+        { :title => "Test title!", :description => "This is a test description", :status => "draft" }
+    }
+
+    let(:valid_session) { {} }
+
+    describe "GET #index" do
+        it "returns a success response" do
+            Article.create! valid_attributes
+            get :index, params: {}, session: valid_session
+
+            # Make sure to swap this as well
+            expect(response).to be_successful # be_successful expects a HTTP Status code of 200
+            # expect(response).to have_http_status(302) # Expects a HTTP Status code of 302
+        end
+    end
+end
