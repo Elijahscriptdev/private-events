0f19644babaa025237d0b367154b31abbce09699
Elijahscriptdev <elijahobominuru@gmail.com>
2020-02-17 21:42:52 +0100

    fixed lint errors

--- spec/requests/signins_spec.rb
+++ spec/requests/signins_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Signins", type: :request do
-  describe "GET /signin" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'Signins', type: :request do
+  describe 'GET /signin' do
+    it 'works! (now write some real specs)' do
       get signin_path
       expect(response).to have_http_status(200)
     end
